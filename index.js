f35c1f7d71c6497401f5bb1809abb814751f5232
Katharine Francis <katie-francis@hotmail.com>
2018-11-30 08:17:26 -0500

    Added web-dev-server

--- index.js
+++ index.js
@@ -7,39 +7,24 @@
 
 import axios from "axios";
 
-const apiUrl =
+const API_URL =
   "https://api.unsplash.com/search/photos?page=1&per_page=50&query=kitten&client_id=592c0cc869cb8a3fa96a46239e298c9aa7cf272768799fe0877882387e0fd727";
 
-// const list = [
-//   "https://picsum.photos/200/?random",
-//   "https://picsum.photos/201/?random",
-//   "https://picsum.photos/202/?random",
-//   "https://picsum.photos/203/?random",
-//   "https://picsum.photos/204/?random",
-//   "https://picsum.photos/205/?random",
-//   "https://picsum.photos/206/?random",
-//   "https://picsum.photos/207/?random",
-//   "https://picsum.photos/208/?random",
-//   "https://picsum.photos/209/?random",
-//   "https://picsum.photos/210/?random",
-//   "https://picsum.photos/211/?random",
-//   "https://picsum.photos/212/?random",
-//   "https://picsum.photos/213/?random",
-//   "https://picsum.photos/214/?random",
-//   "https://picsum.photos/215/?random",
-//   "https://picsum.photos/216/?random",
-//   "https://picsum.photos/217/?random",
-//   "https://picsum.photos/218/?random",
-//   "https://picsum.photos/219/?random"
-// ]; //collection of data
-let pageList = []; //items to display on 'currentPage'.
-let currentPage = 1; //Keep track of where we are in pagination
-const numberPerPage = 4; //number of elements per page
+//number of elements per page
+const NUMBER_PER_PAGE = 10;
+
+const state = {
+  list: [],
+  //items to display on 'currentPage'.
+  pageList: [],
+  //Keep track of where we are in pagination
+  currentPage: 1,
+  maxPages: 1
+};
 
 function start() {
-  console.log("I'm here");
   axios
-    .get(apiUrl, {
+    .get(API_URL, {
       mode: "no-cors",
       headers: {
         "Access-Control-Allow-Origin": "*",
@@ -47,13 +32,16 @@ function start() {
       }
     })
     .then(response => {
+      // change state object, put api data in state
       let list = response.data.results.map(pic => {
-        return pic.urls.small;
+        return pic.urls.thumb;
       });
-      window.list = list;
+
+      //Almost like setState of React, but I'm mutating the state
+      state.list = list;
+
       //Calculate total number of pages
-      let maxPages = Math.ceil(list.length / numberPerPage);
-      window.maxPages = maxPages;
+      state.maxPages = Math.ceil(list.length / NUMBER_PER_PAGE);
       loadList();
     })
     .catch(function(err) {
@@ -66,29 +54,31 @@ window.onload = start;
 
 //Slice chunks of data by the amount per page
 function loadList() {
-  let beginIndex = (currentPage - 1) * numberPerPage;
-  let endIndex = beginIndex + numberPerPage;
+  let beginIndex = (state.currentPage - 1) * NUMBER_PER_PAGE;
+  let endIndex = beginIndex + NUMBER_PER_PAGE;
 
-  pageList = window.list.slice(beginIndex, endIndex);
-  console.log("Pagelist: ", pageList);
+  state.pageList = state.list.slice(beginIndex, endIndex);
+
+  //In React/Redux, the page automatically re-renders when the state is changed, but here I don't have the luxury.
+
   renderList();
   disableButtons();
 }
 
 //Navigating through the Pages
 function nextPage() {
-  currentPage++;
+  state.currentPage++;
   loadList();
 }
 
-window.nextPage = nextPage;
+document.getElementById("next").addEventListener("click", nextPage);
 
 function previousPage() {
-  currentPage--;
+  state.currentPage--;
   loadList();
 }
 
-window.previousPage = previousPage;
+document.getElementById("previous").addEventListener("click", previousPage);
 
 function createNode(element) {
   return document.createElement(element);
@@ -102,7 +92,7 @@ function renderList() {
   let ul = document.getElementById("list");
   ul.innerHTML = "";
 
-  pageList.map(currentEl => {
+  state.pageList.map(currentEl => {
     let li = createNode("li"),
       img = createNode("img");
 
@@ -114,7 +104,7 @@ function renderList() {
 
 function disableButtons() {
   document.getElementById("next").disabled =
-    currentPage === window.maxPages ? true : false;
+    state.currentPage === state.maxPages ? true : false;
   document.getElementById("previous").disabled =
-    currentPage === 1 ? true : false;
+    state.currentPage === 1 ? true : false;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
 const path = require("path");
 
 module.exports = {
+  mode: "production",
   entry: "./index.js",
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist")
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, ".")
   }
 };
