f0ef3920b7cd1de7b37a0b71565d57f731f4e491
Katharine Francis <katie-francis@hotmail.com>
2018-11-30 00:02:45 -0500

    Added webpack and api

--- index.js
+++ index.js
@@ -5,37 +5,60 @@
 //5.) Render data
 //6.) Redraw page controls
 
-const list = [
-  "https://picsum.photos/200/?random",
-  "https://picsum.photos/201/?random",
-  "https://picsum.photos/202/?random",
-  "https://picsum.photos/203/?random",
-  "https://picsum.photos/204/?random",
-  "https://picsum.photos/205/?random",
-  "https://picsum.photos/206/?random",
-  "https://picsum.photos/207/?random",
-  "https://picsum.photos/208/?random",
-  "https://picsum.photos/209/?random",
-  "https://picsum.photos/210/?random",
-  "https://picsum.photos/211/?random",
-  "https://picsum.photos/212/?random",
-  "https://picsum.photos/213/?random",
-  "https://picsum.photos/214/?random",
-  "https://picsum.photos/215/?random",
-  "https://picsum.photos/216/?random",
-  "https://picsum.photos/217/?random",
-  "https://picsum.photos/218/?random",
-  "https://picsum.photos/219/?random"
-]; //collection of data
+import axios from "axios";
+
+const apiUrl =
+  "https://api.unsplash.com/search/photos?page=1&per_page=50&query=kitten&client_id=592c0cc869cb8a3fa96a46239e298c9aa7cf272768799fe0877882387e0fd727";
+
+// const list = [
+//   "https://picsum.photos/200/?random",
+//   "https://picsum.photos/201/?random",
+//   "https://picsum.photos/202/?random",
+//   "https://picsum.photos/203/?random",
+//   "https://picsum.photos/204/?random",
+//   "https://picsum.photos/205/?random",
+//   "https://picsum.photos/206/?random",
+//   "https://picsum.photos/207/?random",
+//   "https://picsum.photos/208/?random",
+//   "https://picsum.photos/209/?random",
+//   "https://picsum.photos/210/?random",
+//   "https://picsum.photos/211/?random",
+//   "https://picsum.photos/212/?random",
+//   "https://picsum.photos/213/?random",
+//   "https://picsum.photos/214/?random",
+//   "https://picsum.photos/215/?random",
+//   "https://picsum.photos/216/?random",
+//   "https://picsum.photos/217/?random",
+//   "https://picsum.photos/218/?random",
+//   "https://picsum.photos/219/?random"
+// ]; //collection of data
 let pageList = []; //items to display on 'currentPage'.
 let currentPage = 1; //Keep track of where we are in pagination
 const numberPerPage = 4; //number of elements per page
-let maxPages = 1; //tells us the total number of pages required to render collection. (this will change if we're doing numeric pagination)
 
 function start() {
-  //Calculate total number of pages
-  maxPages = Math.ceil(list.length / numberPerPage);
-  loadList();
+  console.log("I'm here");
+  axios
+    .get(apiUrl, {
+      mode: "no-cors",
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json"
+      }
+    })
+    .then(response => {
+      let list = response.data.results.map(pic => {
+        return pic.urls.small;
+      });
+      window.list = list;
+      //Calculate total number of pages
+      let maxPages = Math.ceil(list.length / numberPerPage);
+      window.maxPages = maxPages;
+      loadList();
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
 }
 
 //The function that runs on loading the page.
@@ -46,7 +69,7 @@ function loadList() {
   let beginIndex = (currentPage - 1) * numberPerPage;
   let endIndex = beginIndex + numberPerPage;
 
-  pageList = list.slice(beginIndex, endIndex);
+  pageList = window.list.slice(beginIndex, endIndex);
   console.log("Pagelist: ", pageList);
   renderList();
   disableButtons();
@@ -58,10 +81,14 @@ function nextPage() {
   loadList();
 }
 
+window.nextPage = nextPage;
+
 function previousPage() {
   currentPage--;
   loadList();
 }
+
+window.previousPage = previousPage;
 
 function createNode(element) {
   return document.createElement(element);
@@ -87,7 +114,7 @@ function renderList() {
 
 function disableButtons() {
   document.getElementById("next").disabled =
-    currentPage === maxPages ? true : false;
+    currentPage === window.maxPages ? true : false;
   document.getElementById("previous").disabled =
     currentPage === 1 ? true : false;
 }
